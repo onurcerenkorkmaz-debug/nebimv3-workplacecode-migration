eb3a6db99e75931e909ca6410224eb01f9f493b3
onurcerenkorkmaz-debug <onurcerenkorkmaz@gmail.com>
2026-01-06 18:07:01 +0300

    Add WorkplaceCode update-generator script scaffold

---
+++ sql/01_generate_updates.sql
@@ -0,0 +1,10 @@
+
+-- Generates dynamic UPDATE statements for WorkplaceCode migration
+-- 
+-- Parameters:
+--   @OldWorkplaceCode  : Existing WorkplaceCode to be replaced
+--   @NewWorkplaceCode  : New WorkplaceCode to be applied
+--
+-- Output:
+--   Dynamic UPDATE statements for dependent tables
+
